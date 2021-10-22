a9858f2bb4c291a8c9bcba8924f5cc7d75654ea3
Ihor <igor07102002@gmail.com>
2021-10-22 19:26:42 +0200

    test commit

--- src/solutions/lesson6/solution_ihor/Main.kt
+++ src/solutions/lesson6/solution_ihor/Main.kt
@@ -1,5 +1,30 @@
 package solutions.lesson6.solution_ihor
 
+import solutions.lesson6.solution_ihor.task1.ElectroSocket
+import solutions.lesson6.solution_ihor.task1.IPhone
+import solutions.lesson6.solution_ihor.task1.Tesla
+import solutions.lesson6.solution_ihor.task2.decodeWebError
+
 fun main() {
+    decodeWebError(23)
+    decodeWebError(202)
+    decodeWebError(404)
+    decodeWebError(407)
+    decodeWebError(564)
+
+    val phone = IPhone()
+    phone.currentCharge = -1f
+    phone.charge(1f)
+
+    val car = Tesla()
+    car.currentCharge=1f
+    car.charge(5f)
+
+    val phone1 = IPhone()
+    phone1.currentCharge=30f
+    val socket = ElectroSocket(2f)
+    socket.chargeDevice(phone1)
+
+
 
 }
