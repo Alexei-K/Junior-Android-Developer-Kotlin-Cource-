f7a57a66af68a2dc3060cd56723fb3e2bbb47838
Ihor <igor07102002@gmail.com>
2021-10-31 13:52:30 +0100

    Lesson 8 full

---
+++ src/lesson8/solution/ihor/BlackBox.kt
@@ -0,0 +1,13 @@
+package lesson8.solution.ihor
+
+class BlackBox() {
+    // Возвращает случайное число от 1 до 24 null
+    fun getIntOrNull(): Int? {
+        val x = (1..40).random()
+        return if (x < 25) {
+            x
+        } else {
+            null
+        }
+    }
+}
